0fa77cebb4021a27b7b886de64f57512ed339905
HammerSeb <sebastian@hammer-mail.com>
2022-06-10 17:02:37 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from gettext import find
 from setuptools import setup, find_packages
 
 setup(name='XDimer',
-      version='1.0.0',
+      version='1.0.1',
       description='Simulation of multi-molecular emission spectra dominated by intermolecular vibrations ',
       long_description=open('README.md').read(),
       long_description_content_type ='text/markdown' ,
